51f39912becebb6535ae552d91f0fae227b1e52b
MuratErtugral <muratertugral93@gmail.com>
2022-07-01 00:20:26 +0300

    minor bug fixed

--- components/EditMenu.js
+++ components/EditMenu.js
@@ -69,7 +69,7 @@ const EditMenu = ({ id, isPinned, title }) => {
             <svg
               className="w-5 inline mr-2 rotate-[315deg]"
               xmlns="http://www.w3.org/2000/svg"
-              enable-background="new 0 0 24 24"
+              enablBackground="new 0 0 24 24"
               height="24px"
               viewBox="0 0 24 24"
               width="24px"
